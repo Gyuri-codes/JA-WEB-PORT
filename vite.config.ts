0d52d4dd62bff612b0a9402e2bf0c2c4513b3c7f
Gyuri-codes <jericabestano1@gmail.com>
2026-09-06 13:58:59 +0800

    Initial portfolio commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/JA-WEB-PORT/' : './',
+  base: '/JA-WEB-PORT/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
